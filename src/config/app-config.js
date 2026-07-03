7c2b7757ccdc05943127e2f6c0dff89e98e2169b
dimpershko-a11y <dim.pershko@gmail.com>
2026-07-04 00:08:48 +0300

    Add global Hub configuration

---
+++ src/config/app-config.js
@@ -0,0 +1,15 @@
+export const appConfig = {
+  brand: {
+    name: 'PRO-CHISTKA',
+    website: 'pro-chistka.pro',
+    phone: '+7 993 635-60-64'
+  },
+  locale: 'ru-RU',
+  currency: 'RUB',
+  defaultCity: 'Санкт-Петербург',
+  workingHours: {
+    from: '09:00',
+    to: '22:00',
+    days: 'Пн–Вс'
+  }
+};

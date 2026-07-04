575a656be92534716c8eaf0bb7446ddf26951a5f
dimpershko-a11y <dim.pershko@gmail.com>
2026-07-04 12:38:07 +0300

    Add shared event bus

---
+++ src/shared/events/event-bus.js
@@ -0,0 +1,24 @@
+export const eventBus = {
+  listeners: new Map(),
+
+  on(eventName, callback) {
+    const callbacks = this.listeners.get(eventName) || [];
+    callbacks.push(callback);
+    this.listeners.set(eventName, callbacks);
+
+    return () => this.off(eventName, callback);
+  },
+
+  off(eventName, callback) {
+    const callbacks = this.listeners.get(eventName) || [];
+    this.listeners.set(
+      eventName,
+      callbacks.filter((item) => item !== callback)
+    );
+  },
+
+  emit(eventName, payload) {
+    const callbacks = this.listeners.get(eventName) || [];
+    callbacks.forEach((callback) => callback(payload));
+  }
+};
